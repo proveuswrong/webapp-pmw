245d1c556cac04bfe4f9f017b49fdfcf697ac2de
Ferit Tunçer <ferit@cryptolab.net>
2022-04-10 18:53:03 +0300

    trust score

--- src/components/claim.jsx
+++ src/components/claim.jsx
@@ -1,5 +1,6 @@
 import { useParams, useNavigate, useLocation } from "react-router-dom";
-import { getClaim } from "../data";
+import { getClaim, getTrustScore } from "../data";
+import Interval from "react-interval-rerender";
 
 export default function Claim() {
   let params = useParams();
@@ -7,17 +8,41 @@ export default function Claim() {
   let location = useLocation();
   let claim = getClaim(params.id);
 
-  console.debug(params.id);
+  let reRenderInMs = 500;
 
   return (
     <>
       <div>
         <h3>{claim.title}</h3>
-        <p>Category: {claim.category}</p>
-        <p>arbitrator</p>
-        <p>{claim.description}</p>
-        <p>Bounty amount: {claim.amount}</p>
-        <p>Trust score: {claim.accumulatedScore}</p>
+        <p>Category: {claim.category.name}</p>
+        <p>Arbitrator Short Name: {claim.category.arbitrator.shortName}</p>
+        <p>Arbitrator Long Name: {claim.category.arbitrator.shortName}</p>
+        <p>Arbitrator Fee: {claim.category.arbitrator.shortName}</p>
+        <p>
+          Arbitration Fee:{" "}
+          {claim.category.arbitrator.feePerVote * claim.category.jurySize}{" "}
+          {claim.category.arbitrator.currency}
+        </p>
+        <p>Jury Size: {claim.category.jurySize} votes</p>
+        <br />
+        <p>Description: {claim.description}</p>
+        <p>
+          Bounty Amount: {claim.amount.toFixed(3)} {claim.currency}
+        </p>
+        <p>
+          {" "}
+          Trust Score:{" "}
+          <big>
+            <b>
+              <Interval delay={reRenderInMs}>
+                {() => getTrustScore(params.id).slice(0, -3)}
+              </Interval>
+            </b>
+          </big>
+          <Interval delay={reRenderInMs}>
+            {() => getTrustScore(params.id).slice(-3)}
+          </Interval>
+        </p>
 
         <p>
           <button
--- src/data.js
+++ src/data.js
@@ -1,14 +1,85 @@
+let events = {
+  0: {
+    name: "newClaim",
+  },
+  1: {
+    name: "raise",
+  },
+  2: {
+    name: "initiateWithdrawal",
+  },
+  3: {
+    name: "challenge",
+  },
+  4: {
+    name: "appeal",
+  },
+  5: {
+    name: "evidence",
+  },
+  6: {
+    name: "withdrawal",
+  },
+  7: {
+    name: "juryDecision",
+  },
+  8: {
+    name: "debunk",
+  },
+  9: {
+    name: "failedToProveWrong",
+  },
+};
+
+let arbitrators = {
+  0: {
+    shortName: "General",
+    fullName: "General > Blockchain > Technical",
+    feePerVote: 0.025,
+    currency: "ETH",
+  },
+  1: {
+    shortName: "Blockchain",
+    fullName: "General > Blockchain",
+    feePerVote: 0.025,
+    currency: "ETH",
+  },
+  2: {
+    shortName: "Non-Technical",
+    fullName: "General > Blockchain > Non-Technical",
+    feePerVote: 0.025,
+    currency: "ETH",
+  },
+  3: {
+    shortName: "Token Listing",
+    fullName: "General > Blockchain > Non-Technical > Token Listing",
+    feePerVote: 0.07,
+    currency: "ETH",
+  },
+  4: {
+    shortName: "Technical",
+    fullName: "General > Blockchain > Technical",
+    feePerVote: 0.072,
+    currency: "ETH",
+  },
+};
+
+let categories = {
+  0: { name: "Bug Bounty", arbitrator: arbitrators[4], jurySize: 3 },
+};
+
 let claims = [
   {
     id: "0",
     category: categories[0],
     title: "Prove Me Wrong Smart Contracts Are Secure",
     description:
-      "Prove Me Wrong smart contracts are secure. There is no vulnerability.",
+      "Prove Me Wrong smart contracts are secure. There is no vulnerability. Bounty amount is randomized.",
     state: "Live",
-    amount: "3",
-    accumulatedScore: "123123",
-    lastBountyUpdate: Date.now(),
+    amount: Math.random() * 3,
+    currency: "ETH",
+    accumulatedScore: Math.pow(10, Math.floor(Math.random() * 5)) * 1000,
+    lastBountyUpdate: Math.floor(Date.now() / 1000),
     history: [],
   },
 
@@ -19,53 +90,30 @@ let claims = [
     description:
       "Kleros smart contracts are secure. There is no vulnerability.",
     state: "Live",
-    amount: "3",
-    accumulatedScore: "12312312",
-    lastBountyUpdate: Date.now(),
+    amount: 3,
+    currency: "ETH",
+    // accumulatedScore: Math.floor(Math.random() * 1000000000000),
+    accumulatedScore: Math.pow(10, Math.floor(Math.random() * 5)) * 1000,
+    lastBountyUpdate: Math.floor(Date.now() / 1000),
     history: [],
   },
 ];
 
-let categories = {
-  0: { name: "Bug Bounty", arbitrator: arbitrators[4], jurySize: "3" },
-};
-
-let arbitrators = {
-  0: {
-    shortName: "General",
-    fullName: "General > Blockchain > Technical",
-    feePerVote: "0.025",
-    feeCurrency: "ETH",
-  },
-  1: {
-    shortName: "Blockchain",
-    fullName: "General > Blockchain",
-    feePerVote: "0.025",
-    feeCurrency: "ETH",
-  },
-  2: {
-    shortName: "Non-Technical",
-    fullName: "General > Blockchain > Non-Technical",
-    feePerVote: "0.025",
-    feeCurrency: "ETH",
-  },
-  3: {
-    shortName: "Token Listing",
-    fullName: "General > Blockchain > Non-Technical > Token Listing",
-    feePerVote: "0.07",
-    feeCurrency: "ETH",
-  },
-  4: {
-    shortName: "Technical",
-    fullName: "General > Blockchain > Technical",
-    feePerVote: "0.072",
-    feeCurrency: "ETH",
-  },
-};
 export function getClaims() {
   return claims;
 }
 
 export function getClaim(id) {
   return claims.find((claim) => claim.id === id);
+}
+
+export function getTrustScore(id) {
+  const claim = claims.find((claim) => claim.id === id);
+
+  const score = (
+    claim.accumulatedScore +
+    (Math.floor(Date.now() / 1000) - claim.lastBountyUpdate) * claim.amount
+  ).toFixed(0);
+
+  return score;
 }
