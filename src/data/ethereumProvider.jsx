cb561dc61882bb5604d9acff4d6a79e03a1266ff
Ferit <ferit@cryptolab.net>
2022-04-13 00:38:09 +0300

    Update ethereumProvider.jsx

--- src/data/ethereumProvider.jsx
+++ src/data/ethereumProvider.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-export default class EthereumProvider extends React.Component {
+export default class EthereumProvider extends Component {
   constructor(props) {
     super(props);
     this.state = {
