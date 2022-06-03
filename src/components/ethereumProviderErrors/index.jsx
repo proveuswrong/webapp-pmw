d9e733558bb33e99295563f41548e3178f8ee933
Ferit Tunçer <ferit@cryptolab.net>
2022-06-03 03:54:33 +0100

    direct user to a working network

--- src/components/ethereumProviderErrors/index.jsx
+++ src/components/ethereumProviderErrors/index.jsx
@@ -1,20 +1,38 @@
-import {Outlet} from "react-router-dom";
-import {EthereumContext} from "../../data/ethereumProvider";
+import { Outlet } from "react-router-dom";
+import { useEffect, useState, useContext } from "react";
+import { EthereumContext, contractInstances, chains } from "../../data/ethereumProvider";
 
 export default function EthereumProviderErrors() {
+  const ethereumContext = useContext(EthereumContext);
 
   return (
     <section>
-      <EthereumContext.Consumer>
-        {(context) => (
-          <h2>
-            {!context.isDeployedOnThisChain && "This chain id is not supported."}
-            {!context.isProviderDetected && "No Ethereum provider has been detected."}
-
-          </h2>
-        )}
-      </EthereumContext.Consumer>
-      <Outlet/>
+      <h2>
+        {!ethereumContext.isDeployedOnThisChain && "This chain id is not supported."}
+        {!ethereumContext.isProviderDetected && "No Ethereum provider has been detected."}
+      </h2>
+      {!ethereumContext.isDeployedOnThisChain && (
+        <>
+          <p>Try one of these</p>
+          <ul>
+            {Object.keys(contractInstances).map((networkId) => (
+              <li key={networkId}>
+                <button
+                  onClick={() =>
+                    window.ethereum.request({
+                      method: "wallet_switchEthereumChain",
+                      params: [{ chainId: networkId }],
+                    })
+                  }
+                >
+                  {chains[networkId].name}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
+      <Outlet />
     </section>
   );
 }
