99317f81a3d89b90a033624b6689ba4554d2dbd3
Ferit Tunçer <ferit@cryptolab.net>
2022-07-05 13:27:56 +0100

    remove undefined css class

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,45 +1,27 @@
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import ButtonConnect from "/src/components/buttonConnect";
 import ButtonSelectNetwork from "/src/components/buttonSelectNetwork";
 import Logo from "jsx:../../assets/logo.svg";
-
 
 import * as styles from "./index.module.scss";
 
 export default function Header() {
   return (
-    <header className={styles.header}>
-      <Logo className={styles.logo}/>
-      <hr className={styles.hrBelowLogo}/>
+    <header>
+      <Logo className={styles.logo} />
+      <hr className={styles.hrBelowLogo} />
       <div className={styles.subtitle}>Product of Prove Me Wrong</div>
-      <hr/>
+      <hr />
       <nav className={styles.nav}>
         <h2 hide="">Navigation</h2>
-        <NavLink
-
-          to="/"
-        >
-          Home
-        </NavLink>{" "}
-        <NavLink
-
-          to="browse/"
-        >
-          Browse
-        </NavLink>{" "}
-        <NavLink
-          to="create/"
-        >
-          Create
-        </NavLink>
+        <NavLink to="/">Home</NavLink> <NavLink to="browse/">Browse</NavLink> <NavLink to="create/">Create</NavLink>
         <div className={styles.navEthereum}>
-          <ButtonConnect/>
-          <ButtonSelectNetwork/>
+          <ButtonConnect />
+          <ButtonSelectNetwork />
         </div>
       </nav>
 
-      <hr/>
-
+      <hr />
     </header>
   );
 }
