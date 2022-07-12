fe902c5a179f0a3be6a6ad5b6e80665e2ae7ea94
Ferit Tunçer <ferit@cryptolab.net>
2022-07-12 16:56:24 +0100

    refactor home

--- src/routes/home/index.jsx
+++ src/routes/home/index.jsx
@@ -1,15 +1,16 @@
-import {Outlet} from "react-router-dom";
+import { Outlet } from "react-router-dom";
+
+import * as styles from "./index.module.scss";
 
 export default function Home() {
   return (
-    <section>
+    <section className={styles.home}>
       <h1>H1</h1>
       <h2>H2</h2>
       <h3>H3</h3>
       <h4>H4</h4>
       <p>Paragraph</p>
       <small>Small text</small>
-      <Outlet/>
     </section>
   );
 }
